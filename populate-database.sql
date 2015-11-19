72c37f9cc0248e537dffa5d2a17bb174ca493858
Nils Fitinghoff <nils.fitinghoff@gmail.com>
2015-11-19 15:33:48 +0100

    added sql script to add some fruits for testing

---
+++ populate-database.sql
@@ -0,0 +1,15 @@
+INSERT INTO Products (product_name, product_price, 
+					product_desc, product_stock)
+VALUES ('Apple', 12, 'text about apples', 5);
+
+INSERT INTO Products (product_name, product_price, 
+					product_desc, product_stock)
+VALUES ('Pear', 21, 'text about pears', 8);
+
+INSERT INTO Products (product_name, product_price, 
+					product_desc, product_stock)
+VALUES ('Banana', 14, 'text about bananas', 6);
+
+INSERT INTO Products (product_name, product_price, 
+					product_desc, product_stock)
+VALUES ('Pineapples', 33, 'text about pineapples', 3);
