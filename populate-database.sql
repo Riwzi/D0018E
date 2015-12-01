4224e32dc3f8de23f7a53e747fe1037acee4337b
Nils Fitinghoff <nils.fitinghoff@gmail.com>
2015-12-01 20:50:23 +0100

    extended the populate script with an admin login account

--- populate-database.sql
+++ populate-database.sql
@@ -1,15 +1,13 @@
 INSERT INTO Products (product_name, product_price, 
 					product_desc, product_stock)
-VALUES ('Apple', 12, 'text about apples', 5);
+VALUES
+('Apple', 12, 'text about apples', 5),
+('Pear', 21, 'text about pears', 8),
+('Banana', 14, 'text about bananas', 6),
+('Pineapple', 33, 'text about pineapples', 3),
+('Kiwi', 25, 'edible.', 10);
 
-INSERT INTO Products (product_name, product_price, 
-					product_desc, product_stock)
-VALUES ('Pear', 21, 'text about pears', 8);
+insert into shopdb.LoginStaff
+(staff_name, staff_password_sha2_512)
+values ('admin', sha2('admin', 512));
 
-INSERT INTO Products (product_name, product_price, 
-					product_desc, product_stock)
-VALUES ('Banana', 14, 'text about bananas', 6);
-
-INSERT INTO Products (product_name, product_price, 
-					product_desc, product_stock)
-VALUES ('Pineapples', 33, 'text about pineapples', 3);
