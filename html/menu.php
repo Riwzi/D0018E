8c3c807052aa3e36d9d72db52a4a68a9a552dbb1
Nils Fitinghoff <nils.fitinghoff@gmail.com>
2015-12-08 18:07:28 +0100

    Add login button to menu

--- html/menu.php
+++ html/menu.php
@@ -1,2 +1,8 @@
 <a href='/'><button>Home</button></a>
 <a href='/basket/'><button>Basket</button></a>
+<?php
+//only show login button if the customer isn't already logged in
+if (!array_key_exists('customer_login', $_SESSION)){
+    echo "<a href='/login/'><button>Login</button></a>";
+}
+?>
