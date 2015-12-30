6df92a281fe01ea0385f24ce2a39917eb8b236e5
Nils Fitinghoff <nils.fitinghoff@gmail.com>
2015-12-30 17:30:43 +0100

    finish comment submission

--- html/products/comment.php
+++ html/products/comment.php
@@ -1,12 +1,16 @@
 <?php
 session_start();
+/**/
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+/**/
 require "../login/require_login.php";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // some input validation might be needed
     $user = $_SESSION["customer_login"];
     $product_id = $_REQUEST["product_id"];
     $comment = $_REQUEST["comment"];
-    if(array_key_exists($_REQUEST, "response_to")){
+    if(array_key_exists("response_to", $_REQUEST)){
         $parent_id = $_REQUEST["response_to"];
     } else {
         $parent_id = NULL;
@@ -14,14 +18,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // find the customer id of the commenter
     require "../dbconnect.php";
     $conn = dbconnect();
-    $stmt = $conn->prepare("SELECT customer_id FROM LoginCustomer WHERE customer_email=?");
+    $stmt = $conn->prepare("SELECT customer_id FROM shopdb.Customers WHERE customer_email=?");
+    //var_dump($conn->error_list);
     $stmt->bind_param("s", $user);
     $stmt->execute();
     $stmt->bind_result($customer_id);
-    $stmt->fetch()
-    // TODO what if customer email is not in the db?
-    var_dump($customer_id);
-    /*
+    $stmt->fetch();
     $stmt->close();
     // insert the comment into the database
     $stmt = $conn->prepare("INSERT INTO shopdb.ProductComments
